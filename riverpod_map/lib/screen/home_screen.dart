8760624035334a16172567553207d399c432032f
Nguyen Huy Hoang <19t6n2003@gmail.com>
2024-08-11 10:40:16 +0700

    create git repo

---
+++ riverpod_map/lib/screen/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_map/screen/map_controller_screen.dart';
+import 'package:riverpod_map/screen/polyline_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Map Package'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const PolylineScreen()));
+                },
+                child: const Text('Polyline Markers Page')),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const MapControllerScreen()));
+                },
+                child: const Text('Map Controller Page')),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ riverpod_map/lib/screen/polyline_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PolylineScreen extends StatelessWidget {
+  const PolylineScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Polyline maker Screen'),),
+      body: const Placeholder(),
+    );
+  }
+}
