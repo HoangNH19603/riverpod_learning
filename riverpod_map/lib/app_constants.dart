687016d78e676ef1111e8e36ed4a791b5ee5c4a3
Nguyen Huy Hoang <19t6n2003@gmail.com>
2024-08-13 11:03:30 +0700

    update: modifier app constants update secret keys

--- riverpod_map/lib/app_constants.dart
+++ riverpod_map/lib/app_constants.dart
@@ -1,17 +1,33 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 enum Mapbox {
-  uri(path: 'api.mapbox.com'),
-  standardStyle(path: 'mapbox://styles/mapbox/standard'),
-  standardSatelliteStyle(path: 'mapbox://styles/mapbox/standard-satellite'),
-  streetsStyle(path: 'mapbox://styles/mapbox/streets-v12'),
-  outdoorsStyle(path: 'mapbox://styles/mapbox/outdoors-v12'),
-  lightStyle(path: 'mapbox://styles/mapbox/light-v11'),
-  darkStyle(path: 'mapbox://styles/mapbox/dark-v11'),
-  satelliteStyle(path: 'mapbox://styles/mapbox/satellite-v9'),
-  satelliteStreetsStyle(path: 'mapbox://styles/mapbox/satellite-streets-v12'),
-  navigationDayStyle(path: 'mapbox://styles/mapbox/navigation-day-v1'),
-  navigationNightStyle(path: 'mapbox://styles/mapbox/navigation-night-v1');
+  // ${dotenv.env['ACCESS_TOKEN']}
+  urlTemplate(value: 'https://api.mapbox.com/styles/v1/hoanghn190603/clzrta6wf007601qu2ce63xo8/tiles/256/{z}/{x}/{y}@2x?access_token='),
+  standardStyle(value: 'mapbox://styles/mapbox/standard'),
+  standardSatelliteStyle(value: 'mapbox://styles/mapbox/standard-satellite'),
+  streetsStyle(value: 'mapbox://styles/mapbox/streets-v12'),
+  outdoorsStyle(value: 'mapbox://styles/mapbox/outdoors-v12'),
+  lightStyle(value: 'mapbox://styles/mapbox/light-v11'),
+  darkStyle(value: 'mapbox://styles/mapbox/dark-v11'),
+  satelliteStyle(value: 'mapbox://styles/mapbox/satellite-v9'),
+  satelliteStreetsStyle(value: 'mapbox://styles/mapbox/satellite-streets-v12'),
+  navigationDayStyle(value: 'mapbox://styles/mapbox/navigation-day-v1'),
+  navigationNightStyle(value: 'mapbox://styles/mapbox/navigation-night-v1');
 
-  final String path;
+  final String value;
 
-  const Mapbox({required this.path});
+  const Mapbox({required this.value});
+
+  // add getters
+  String get urlTemplate => value;
+  String get standardStyle => value;
+  String get standardSatelliteStyle => value;
+  String get streetsStyle => value;
+  String get outdoorsStyle => value;
+  String get lightStyle => value;
+  String get darkStyle => value;
+  String get satelliteStyle => value;
+  String get satelliteStreetsStyle => value;
+  String get navigationDayStyle => value;
+  String get navigationNightStyle => value;
 }
--- riverpod_map/lib/screen/polyline_screen.dart
+++ riverpod_map/lib/screen/polyline_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:riverpod_map/app_constants.dart';
 
 class PolylineScreen extends StatelessWidget {
   const PolylineScreen({super.key});
@@ -7,7 +9,13 @@ class PolylineScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Polyline maker Screen'),),
-      body: const Placeholder(),
+      body: FlutterMap(
+        children: [
+          TileLayer(
+            urlTemplate: Mapbox.urlTemplate,
+          )
+        ]
+        ),
     );
   }
 }
