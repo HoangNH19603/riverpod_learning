1beafcc31046a9a0173d8606f5337e3d6cd178de
Nguyen Huy Hoang <19t6n2003@gmail.com>
2024-11-21 00:03:02 +0700

    feature: add navigation

--- riverpod_map/android/app/src/main/kotlin/com/example/riverpod_map/MainActivity.kt
+++ riverpod_map/android/app/src/main/kotlin/com/example/riverpod_map/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.riverpod_map
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity()
 
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
